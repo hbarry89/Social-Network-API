7879b43aeb0c22fa5e02968738ca290406563558
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-03-19 12:34:58 -0400

    create user model

--- models/User.js
+++ models/User.js
@@ -1,27 +1,41 @@
-// User:
+const mongoose = require('mongoose');
 
-// username
-
-// String
-// Unique
-// Required
-// Trimmed
-
-// email
-
-// String
-// Required
-// Unique
-// Must match a valid email address (look into Mongoose's matching validation)
-
-// thoughts
-
-// Array of _id values referencing the Thought model
-
-// friends
-
-// Array of _id values referencing the User model (self-reference)
-
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    // Must match a valid email address (look into Mongoose's matching validation)
+  },
+  thoughts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+},
+{
+  toJSON: {
+    virtuals: true
+  },
+  id: false
+});
 
 // Schema Settings:
 // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
